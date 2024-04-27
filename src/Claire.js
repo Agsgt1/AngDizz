ba4cad61296c6d275dad05ee29805531ddb1eb8f
Agsgt1 <138738952+Agsgt1@users.noreply.github.com>
2024-04-26 21:30:14 -0600

    Create Claire.js
    omg claire is the best student in fpe

---
+++ src/Claire.js
@@ -0,0 +1,15 @@
+//CLAIRE//
+export var nameC = "claire"
+export var meme = "PEDRO PEDRO PEDRO"
+export function fail(testname) {
+  alert("CONGRATS YOU FAILED", testname);
+}
+export function mexicate(mexite) {
+  alert("nop you mexican", mexite)
+}
+
+//CLAIRE'S FRIENDS//
+export function control(name) {
+  alert("NO")
+}
+export var tres = "Threshold"
